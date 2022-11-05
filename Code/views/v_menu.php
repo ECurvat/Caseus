9286f1b84412324b7d6b0953b7708ecff678cf2c
Elliot Curvat <91797716+ECurvat@users.noreply.github.com>
2022-11-05 15:52:15 +0100

    corrections menu

--- Code/views/v_menu.php
+++ Code/views/v_menu.php
@@ -18,8 +18,9 @@ $positionCourante = $_SESSION['compte']->getPosition();
         <div class="subcategory <?php echo ($page=='gestion-echanges' ? "active":'')?>">Échanges</div>
     <?php endif; ?>
     <?php if ($positionCourante == 'MANA'): ?>
-        <div class="subcategory <?php echo ($page=='gestion-horaires' ? "active":'')?>"><a href="index.php?page=gestionhoraires">Horaires</a></div>
+        <div class="subcategory <?php echo ($page=='gestion-horaires' ? "active":'')?>"><a href="index.php?page=gestion-horaires">Horaires</a></div>
         <div class="subcategory <?php echo ($page=='gestion-conges' ? "active":'')?>">Congés</div>
+        <div class="subcategory <?php echo ($page=='gestion-employes' ? "active":'')?>">Employés</div>
         <div class="subcategory <?php echo ($page=='statistiques' ? "active":'')?>">Statistiques</div>
     <?php endif; ?>
 </navbar>
