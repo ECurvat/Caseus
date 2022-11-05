e0d042599b7dfce3998b14d555c69160032754b1
Elliot Curvat <91797716+ECurvat@users.noreply.github.com>
2022-11-05 15:34:37 +0100

    contrainte (dispo sur même jour)

--- Code/models/disponibilites.php
+++ Code/models/disponibilites.php
@@ -12,8 +12,10 @@ if (isset($_POST['ajouter']) &&
     isset($_POST['ajoutDebut']) &&
     isset($_POST['ajoutFin'])) {
     // ici, c'est le formulaire d'ajout qui a été envoyé
-    if ($_POST['ajoutDebut'] > $_POST['ajoutFin']) $alert = choixAlert('contraintes');
-    else {
+    if (($_POST['ajoutDebut'] > $_POST['ajoutFin']) ||
+    (explode("T", $_POST['ajoutDebut'])[0] != explode("T", $_POST['ajoutFin'])[0]))  {
+        $alert = choixAlert('contraintes');
+    } else {
         $paramsAjout = array($_SESSION['compte']->getId(), $_POST['ajoutDebut'], $_POST['ajoutFin']);
         $disponibiliteDAO->ajouterDispo($paramsAjout);
         $alert = choixAlert('succes_operation');
@@ -22,8 +24,10 @@ if (isset($_POST['ajouter']) &&
 
 if (isset($_POST['modifier'])) {
     // ici, c'est le formulaire pour modifier une dispo qui a été envoyé
-    if ($_POST['modifDebut'] > $_POST['modifFin']) $alert = choixAlert('contraintes');
-    else {
+    if (($_POST['modifDebut'] > $_POST['modifFin']) ||
+    (explode("T", $_POST['modifDebut'])[0] != explode("T", $_POST['modifFin'])[0]))  {
+        $alert = choixAlert('contraintes');
+    } else {
         $dispoModifiee = new Disponibilite($_POST['modifId'], $_SESSION['compte']->getId(), $_POST['modifDebut'], $_POST['modifFin']);
         $disponibiliteDAO->modifierDispoParId($dispoModifiee);
         $alert = choixAlert('succes_operation');
