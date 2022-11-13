9bfbcdff863b41d39fd7077a202fca83165290e1
Elliot Curvat <91797716+ECurvat@users.noreply.github.com>
2022-11-13 17:22:57 +0100

    fonction pour mot de passe aléatoire

--- Code/controllers/c_gestion-employes.php
+++ Code/controllers/c_gestion-employes.php
@@ -1,7 +1,26 @@
 <?php
+
+
+// pas encore utilisé : à voir en séance SAÉ
+function randomPassword() {
+
+    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    $pass = array(); 
+    $alphaLength = strlen($alphabet) - 1; 
+    for ($i = 0; $i < 8; $i++) { // mot de passe de 8 caractères
+        $n = rand(0, $alphaLength);
+        $pass[] = $alphabet[$n];
+    }
+    return implode($pass); // array -> string
+
+}
+
 if ($_SESSION['compte']->getPosition() != 'MANA') {
+
     header("Location: index.php");
     exit;
+
 }
 require_once(PATH_MODELS.$page.'.php'); 
+
 require_once(PATH_VIEWS.$page.'.php'); 
