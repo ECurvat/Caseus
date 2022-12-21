7953c6631b26bbe74ad4dd17eca2aaaff6f51a00
Elliot Curvat <91797716+ECurvat@users.noreply.github.com>
2022-12-21 11:50:16 +0100

    initialisation liste des services dispo pour polyvalents

--- Code/models/gestion-horaires.php
+++ Code/models/gestion-horaires.php
@@ -5,12 +5,32 @@ $absenceDAO = new AbsenceDAO(true);
 $listeAbsences = $absenceDAO->getAbsenceParDate($para);
 require_once(PATH_MODELS . 'EmployeDAO.php');
 $employeDAO = new EmployeDAO(true);
+require_once(PATH_MODELS . 'ServiceDAO.php');
+$serviceDAO = new ServiceDAO(true);
 
 // Partie principale : création des emplois du temps
 $semTest = 49;
 $anTest = 2022;
 //                                                      Gestion des employés polyvalents
+$srvPoly = array();
+for($i = 0; $i<7; $i++) {
+    $srvPoly[$i] = array();
+    // on crée un tableau avec les services pour chaque jour de la semaine
+    $listeServices = $serviceDAO->getListeServices();
+    foreach ($listeServices as $service) {
+        if (date('h:i:s', strtotime($service->getFin()) - strtotime($service->getDebut())) == '04:30:00') {
+            // alors c'est un service de polyvalent
+            for($j = 0; $j<$service->getNombre(); $j++) {
+                array_push($srvPoly[$i], $service);
+            }
+        }
+    }
+}
 $listePoly = $employeDAO->getEmployesParRang('POLY');
+$nbSrvPoly = array();
+foreach ($listePoly as $elem) {
+    $nbSrvPoly[$elem->getId()] = 0;
+}
 // on trouve le premier jour de la semaine et le dernier jour de la semaine
 $jourCourant = new DateTime(date('Y-m-d',strtotime($anTest.'W'.$semTest)));
 // on boucle sur tous les jours de la semaine
@@ -24,6 +44,19 @@ for($i = 0; $i<7; $i++) {
         // on supprime les polyvalents qui n'ont pas d'absence du tableau
         if($listeAbsPoly[$poly->getId()] == null) {
             unset($listeAbsPoly[$poly->getId()]);
+        }
+    }
+
+    if (!empty($listeAbsPoly)) {
+        // on va traiter les absences qu'il y a
+        // si le polyvalent a une absence, on va regarder s'il peut faire un service de matin
+        // si il peut on lui met, sinon on regarde si son absence lui permet de faire un service de soir
+        // si il peut on lui met, sinon rien
+        foreach ($listeAbsPoly as $idEmp => $tabAbs) {
+            // dans abs, on a un tableau d'absences pour le polyvalent qu'on regarde
+            foreach ($tabAbs as $nbAbs => $abs) {
+                print_r($abs);
+            }
         }
     }
     $jourCourant->modify('+1 day');
