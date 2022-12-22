ffbf1697575bd2dce8953b48d80d14b4fcc2cf4b
Elliot Curvat <91797716+ECurvat@users.noreply.github.com>
2022-12-22 11:28:57 +0100

    affectation service des absents

--- Code/models/gestion-horaires.php
+++ Code/models/gestion-horaires.php
@@ -29,7 +29,7 @@ for($i = 0; $i<7; $i++) {
 $listePoly = $employeDAO->getEmployesParRang('POLY');
 $nbSrvPoly = array();
 foreach ($listePoly as $elem) {
-    $nbSrvPoly[$elem->getId()] = 0;
+    $nbSrvPoly[$elem->getId()] = array(0, 0, 0, 0, 0, 0, 0);
 }
 // on trouve le premier jour de la semaine et le dernier jour de la semaine
 $jourCourant = new DateTime(date('Y-m-d',strtotime($anTest.'W'.$semTest)));
@@ -55,9 +55,35 @@ for($i = 0; $i<7; $i++) {
         foreach ($listeAbsPoly as $idEmp => $tabAbs) {
             // dans abs, on a un tableau d'absences pour le polyvalent qu'on regarde
             foreach ($tabAbs as $nbAbs => $abs) {
-                print_r($abs);
+                // le formattage ci-dessous permet d'enlever la date et de ne garder que l'heure
+                $da = date("H:i:s", strtotime($abs->getDebut()));
+                $fa = date("H:i:s", strtotime($abs->getFin()));
+                echo '<BR>DA  //////////   DS  ////////////////////////////////////////////  FA   ///////////    FS';
+                foreach ($srvPoly[$i] as $srv) {
+                    $ds = date("H:i:s", strtotime($srv->getDebut()));
+                    $fs = date("H:i:s", strtotime($srv->getFin()));
+                    echo '<br>        ' . $da . " et " . $ds . "     //////////    " . $fa . " et " . $fs;
+                    if (strtotime($da) < strtotime($ds) && strtotime($fa) < strtotime($ds) && $nbSrvPoly[$abs->getIdEmploye()][$i] == 0) {
+                        $nbSrvPoly[$abs->getIdEmploye()][$i] = 1;
+                        if(($key = array_search($srv, $srvPoly[$i], TRUE)) !== FALSE) {
+                            unset($srvPoly[$i][$key]);
+                        }
+                        echo 'DA < DS et FA < DS';
+                    }
+                    if (strtotime($ds) < strtotime($da) && strtotime($fs) < strtotime($da) && $nbSrvPoly[$abs->getIdEmploye()][$i] == 0) {
+                        $nbSrvPoly[$abs->getIdEmploye()][$i] = 1;
+                        if(($key = array_search($srv, $srvPoly[$i], TRUE)) !== FALSE) {
+                            unset($srvPoly[$i][$key]);
+                        }
+                        echo 'DS < DA et FS < DA';
+                    }
+                }
             }
         }
+        echo '<pre>';
+        print_r($srvPoly[$i]);
+        echo '</pre>';
     }
     $jourCourant->modify('+1 day');
 }
+
