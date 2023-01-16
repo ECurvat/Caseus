da41609cc4bf6592ec03b26c7169b5a91aac1587
Elliot Curvat <91797716+ECurvat@users.noreply.github.com>
2023-01-16 15:07:20 +0100

    état d'un planning retiré

--- Code/entities/Planning.php
+++ Code/entities/Planning.php
@@ -1,36 +1,59 @@
 <?php
+/**
+ * Classe Planning :
+ * Planning d'un employé
+ */
 class Planning {
 
     private $id_planning;
     private $id_employe;
-    private $id_etat;
     private $n_semaine;
     private $annee_planning;
 
-    public function __construct($id_planning, $id_employe, $id_etat, $n_semaine, $annee_planning) {
+    /**
+     * Constructeur de la classe Planning
+     * @param int $id_planning
+     * @param int $id_employe
+     * @param int $n_semaine
+     * @param int $annee_planning
+     * @see Employe
+     */
+    public function __construct($id_planning, $id_employe, $n_semaine, $annee_planning) {
         $this->id_planning = $id_planning;
         $this->id_employe = $id_employe;
-        $this->id_etat = $id_etat;
         $this->n_semaine = $n_semaine;
         $this->annee_planning = $annee_planning;
     }
 
+    /**
+     * Getter de l'id du planning
+     * @return int
+     */
     public function getIdPlanning() {
         return $this->id_planning;
     }
 
+    /**
+     * Getter de l'id de l'employé
+     * @see Employe
+     * @return int
+     */
     public function getIdEmploye() {
         return $this->id_employe;
     }
 
-    public function getIdEtat() {
-        return $this->id_etat;
-    }
-
+    /**
+     * Getter du numéro de la semaine du planning
+     * @return int
+     */
     public function getNSemaine() {
         return $this->n_semaine;
     }
 
+    /**
+     * Getter de l'année du planning
+     * @return int
+     */
     public function getAnneePlanning() {
         return $this->annee_planning;
     }
--- Code/models/PlanningDAO.php
+++ Code/models/PlanningDAO.php
@@ -6,14 +6,14 @@ class PlanningDAO extends DAO {
     public function getPlanningParEmp($para) {
         $result = $this->queryRow('SELECT * FROM PLANNING WHERE (ID_EMPLOYE = ?) AND (N_SEMAINE = ?) AND (ANNEE_PLANNING = ?)', $para);
         if ($result) {
-            $planning = new Planning($result[0], $result[1], $result[2], $result[3], $result[4]);
+            $planning = new Planning($result[0], $result[1], $result[2], $result[3]);
             return $planning;
         }
         return null;
     }
 
     public function addPlanning($para) {
-        $result = $this->queryRow('INSERT INTO PLANNING (ID_EMPLOYE, ID_ETAT, N_SEMAINE, ANNEE_PLANNING) VALUES (?, 1, ?, ?)', $para);
+        $result = $this->queryRow('INSERT INTO PLANNING (ID_EMPLOYE, N_SEMAINE, ANNEE_PLANNING) VALUES (?, ?, ?)', $para);
         return $result;
     }
     public function removePlanning($id) {
@@ -26,7 +26,7 @@ class PlanningDAO extends DAO {
         if ($result) {
             $listePlannings = array();
             foreach ($result as $elem) {
-                $planning = new Planning($elem[0], $elem[1], $elem[2], $elem[3], $elem[4]);
+                $planning = new Planning($elem[0], $elem[1], $elem[2], $elem[3]);
                 array_push($listePlannings, $planning);
             }
             return $listePlannings;
@@ -39,7 +39,7 @@ class PlanningDAO extends DAO {
         if ($result) {
             $listePlannings = array();
             foreach ($result as $elem) {
-                $planning = new Planning($elem[0], $elem[1], $elem[2], $elem[3], $elem[4]);
+                $planning = new Planning($elem[0], $elem[1], $elem[2], $elem[3]);
                 array_push($listePlannings, $planning);
             }
             return $listePlannings;
@@ -50,7 +50,7 @@ class PlanningDAO extends DAO {
     public function getPlanningParId($id) {
         $result = $this->queryRow('SELECT * FROM PLANNING WHERE ID_PLANNING = ?', array($id));
         if ($result) {
-            $planning = new Planning($result[0], $result[1], $result[2], $result[3], $result[4]);
+            $planning = new Planning($result[0], $result[1], $result[2], $result[3]);
             return $planning;
         }
         return null;
